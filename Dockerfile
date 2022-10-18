2a7363702905f6ffad06f4acd51fd78ae6855923
yuji naramoto <yuji.naramoto@playpark.work>
2022-10-19 05:38:12 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.8.13
+WORKDIR /backend/src
+COPY requirements.txt ./
+RUN apt-get update &&\
+    apt-get install libgl1-mesa-dev -y &&\
+    pip install -r requirements.txt
+COPY src ./
+CMD ["flask", "run", "--host", "0.0.0.0"]
---
+++ src/emotion.py
@@ -0,0 +1,15 @@
+import cv2
+import base64
+import numpy as np
+from deepface import DeepFace
+
+def get_emotion(img_base64):
+    #バイナリデータ <- base64でエンコードされたデータ  
+    img_binary = base64.b64decode(img_base64)
+    jpg=np.frombuffer(img_binary,dtype=np.uint8)
+
+    #raw image <- jpg
+    img = cv2.imdecode(jpg, flags=cv2.IMREAD_COLOR)
+    result = DeepFace.analyze(img,actions=['emotion'])
+
+    return result
